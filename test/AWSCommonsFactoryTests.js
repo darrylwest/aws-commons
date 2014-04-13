b986ffb177493bcf013ae86d7235c2be8ccacd85
darryl west <darryl.west@RainCitySoftware.com>
2014-04-12 17:42:15 -0700

    added factory methods

--- test/AWSCommonsFactoryTests.js
+++ test/AWSCommonsFactoryTests.js
@@ -5,30 +5,50 @@
  * @created: 4/12/14 4:56 PM
  */
 var should = require('chai').should(),
+    dash = require('lodash'),
     log = require('simple-node-logger' ).createLogger(),
     AWSCommonsFactory = require('../lib/AWSCommonsFactory');
 
 describe('AWSCommonsFactory', function() {
     'use strict';
 
-
-
     var createOptions = function() {
         var opts = {};
 
+        opts.environment = 'test';
         opts.log = log;
-        opts.base64Keys = 'ya'; // TODO create a simulated keys
+        // opts.base64Keys = 'ya'; // TODO create a simulated keys
 
         return opts;
     };
 
     describe('#instance', function() {
-        var factory = new AWSCommonsFactory( createOptions() );
+        var methods = [
+            'createS3Connection'
+        ];
 
         it('should create an instance of AWSCommonsFactory', function() {
+            var factory = new AWSCommonsFactory( createOptions() );
             should.exist( factory );
-            
+
             factory.should.be.an.instanceof( AWSCommonsFactory );
+        });
+
+        it('should create an instance of AWSCommonsFactory from static constructor', function() {
+            var factory = AWSCommonsFactory.newInstance( createOptions() );
+
+            should.exist( factory );
+
+            factory.should.be.an.instanceof( AWSCommonsFactory );
+        });
+
+        it('should have all known methods by size and name', function() {
+            var factory = new AWSCommonsFactory( createOptions() );
+
+            dash.methods( factory ).length.should.equal( methods.length );
+            methods.forEach(function(method) {
+                factory[ method ].should.be.a( 'function' );
+            });
         });
     });
 });
